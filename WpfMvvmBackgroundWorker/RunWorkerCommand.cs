d249cad9c2c53667d008709b15d6bc280efee92d
Michal Gajda <michal@gajda.co.uk>
2019-05-20 20:09:59 +0100

    Init

---
+++ WpfMvvmBackgroundWorker/RunWorkerCommand.cs
@@ -0,0 +1,29 @@
+﻿namespace WpfMvvmBackgroundWorker
+{
+    using System;
+    using System.ComponentModel;
+    using System.Windows.Input;
+
+    internal sealed class RunWorkerCommand : ICommand
+    {
+        private readonly BackgroundWorker backgroundWorker;
+
+        public RunWorkerCommand(BackgroundWorker backgroundWorker)
+        {
+            this.backgroundWorker = backgroundWorker;
+        }
+
+        public bool CanExecute(object parameter) => !this.backgroundWorker.IsBusy;
+
+        public void Execute(object parameter)
+        {
+            this.backgroundWorker.RunWorkerAsync();
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+    }
+}
---
+++ WpfMvvmBackgroundWorker/ShellViewModel.cs
@@ -0,0 +1,71 @@
+﻿namespace WpfMvvmBackgroundWorker
+{
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+    using System.Threading;
+    using System.Windows.Input;
+
+    internal sealed class ShellViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private readonly BackgroundWorker backgroundWorker = new BackgroundWorker { WorkerReportsProgress = true, };
+
+        private ICommand runWorkerCommand;
+
+        private int progressPercentage;
+
+        private string userState = "Ready";
+
+        public int ProgressPercentage
+        {
+            get => this.progressPercentage;
+            set
+            {
+                this.progressPercentage = value;
+                this.NotifyPropertyChanged();
+            }
+        }
+
+        public string UserState
+        {
+            get => this.userState;
+            set
+            {
+                this.userState = value;
+                this.NotifyPropertyChanged();
+            }
+        }
+
+        public ICommand RunWorkerCommand => this.runWorkerCommand ??
+                                            (this.runWorkerCommand = new RunWorkerCommand(this.backgroundWorker));
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public ShellViewModel()
+        {
+            this.backgroundWorker.DoWork += (sender, e) =>
+            {
+                var worker = sender as BackgroundWorker;
+                for (var i = 0; i < 100; i++)
+                {
+                    Thread.Sleep(60);
+                    worker?.ReportProgress(i, $"{i}%");
+                }
+            };
+            this.backgroundWorker.ProgressChanged += (sender, e) =>
+            {
+                this.ProgressPercentage = e.ProgressPercentage;
+                this.UserState = (string)e.UserState;
+            };
+            this.backgroundWorker.RunWorkerCompleted += (sender, e) =>
+            {
+                this.UserState = "Done";
+                CommandManager.InvalidateRequerySuggested();
+            };
+        }
+    }
+}
